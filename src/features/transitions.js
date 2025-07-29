4bf696e40b6d2f6f2655e5111b21fb87c3f7b21e
Chinmay <signups@chinmay.design>
2025-07-29 14:08:05 +0100

    Barba setup complete

--- src/features/transitions.js
+++ src/features/transitions.js
@@ -5,18 +5,31 @@ function transition() {
   console.log(barba)
   console.log(gsap)
 
+  gsap.to('.loader-bar', {
+    yPercent: 100,
+    stagger: 0.05,
+    duration: 1,
+  })
+
   barba.init({
     transitions: [
       {
         name: 'opacity-transition',
         leave(data) {
-          return gsap.to(data.current.container, {
-            opacity: 0,
+          console.log('leaving', data)
+          return gsap.to('.loader-bar', {
+            yPercent: 0,
+            stagger: 0.05,
+            duration: 1,
           })
         },
         enter(data) {
-          return gsap.from(data.next.container, {
-            opacity: 0,
+          console.log('entering')
+          data.current.container.remove()
+          return gsap.to('.loader-bar', {
+            yPercent: 100,
+            stagger: 0.05,
+            duration: 1,
           })
         },
       },
