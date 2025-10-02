f0862efc0f0bfac46dd5e11125819a1b7a3dc0aa
Chinmay <signups@chinmay.design>
2025-10-02 14:45:23 +1000

    Home page nav interaction
    - Project properties show and hide on hover
    - Project preview rotation and translation is mapped to X position of the mouse
    - A few bugs have popped up. See Notion issues.

--- src/features/projectNav.js
+++ src/features/projectNav.js
@@ -1,35 +1,56 @@
 import gsap from 'gsap'
 
 const projectNav = () => {
-  const projectList = document.querySelectorAll('.project')
+  const navHover = () => {
+    const projectList = document.querySelectorAll('.project')
 
-  projectList.forEach((project) => {
-    const projectName = project.querySelector('h2')
-    const projectNameItalic = project.querySelector('.project-name-italic')
-    const projectPropertyList = project.querySelectorAll('.project-property')
+    projectList.forEach((project) => {
+      const projectName = project.querySelector('h2')
+      const projectNameItalic = project.querySelector('.project-name-italic')
+      const projectPropertyList = project.querySelectorAll('.project-property')
 
-    project.addEventListener('mouseenter', () => {
-      const TLmouseEnter = gsap.timeline({
-        defaults: { duration: 0.6, ease: 'power3.out' },
+      project.addEventListener('mouseenter', () => {
+        const TLmouseEnter = gsap.timeline({
+          defaults: { duration: 0.6, ease: 'power3.out' },
+        })
+        TLmouseEnter.to(projectName, { y: -22 }, 0)
+          .to(projectNameItalic, { y: -22 }, 0)
+          .to(projectPropertyList, { top: 0, stagger: { amount: 0.15 } }, 0)
       })
-      TLmouseEnter.to(projectName, { y: -22 }, 0)
-        .to(projectNameItalic, { y: -22 }, 0)
-        .to(projectPropertyList, { top: 0, stagger: { each: 0.11 } }, 0)
-    })
 
-    project.addEventListener('mouseleave', () => {
-      const TLmouseLeave = gsap.timeline({
-        defaults: { duration: 0.6, ease: 'power3.out' },
+      project.addEventListener('mouseleave', () => {
+        const TLmouseLeave = gsap.timeline({
+          defaults: { duration: 0.6, ease: 'power3.inOut' },
+        })
+        TLmouseLeave.to(projectName, { y: 0 }, 0.15)
+          .to(projectNameItalic, { y: 0 }, 0.15)
+          .to(
+            projectPropertyList,
+            { top: -18, stagger: { amount: 0.15, from: 'end' } },
+            0
+          )
       })
-      TLmouseLeave.to(projectName, { y: 0 }, 0.35)
-        .to(projectNameItalic, { y: 0 }, 0.35)
-        .to(
-          projectPropertyList,
-          { top: -18, stagger: { each: 0.11, from: 'end' } },
-          0
-        )
     })
-  })
+  }
+
+  const translateProjectPreview = () => {
+    const mapperMovement = gsap.utils.mapRange(0, window.innerWidth, 15, -15)
+    const mapperRotation = gsap.utils.mapRange(0, window.innerWidth, -2, 2)
+
+    document.addEventListener('mousemove', (e) => {
+      const mouseX = e.clientX
+      const translateionAmount = mapperMovement(mouseX)
+      const rotationAmount = mapperRotation(mouseX)
+      gsap.to('.project-preview-wrapper', {
+        translateX: translateionAmount,
+        rotateY: rotationAmount,
+        duration: 0.5,
+      })
+    })
+  }
+
+  navHover()
+  translateProjectPreview()
 }
 
 export default projectNav
