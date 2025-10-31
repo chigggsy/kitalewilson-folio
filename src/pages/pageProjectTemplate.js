d71982feda78a295f13fb9e69f34f352c294a5a3
Chinmay <signups@chinmay.design>
2025-10-31 13:16:52 +1100

    Set up HLS support (ASR page only)

--- src/pages/pageProjectTemplate.js
+++ src/pages/pageProjectTemplate.js
@@ -1,9 +1,11 @@
 import gsap from 'gsap'
 import { SplitText } from 'gsap/SplitText'
+import Hls from 'hls.js'
+
+console.log(Hls)
 
 const pageProjectTemplate = () => {
   const mm = gsap.matchMedia()
-
   const nextProject = document.querySelector('.project-navigation.is-next')
   const prevProject = document.querySelector('.project-navigation.is-previous')
 
@@ -46,16 +48,36 @@ const pageProjectTemplate = () => {
       type: 'words',
     })
 
-    const tl = gsap.timeline({
+    const tl_projectNav = gsap.timeline({
       defaults: { duration: 0.6, ease: 'power3.out', stagger: 0.02 },
     })
-    tl.to(st_projectName.words, { y: -22 }, 0).to(
+    tl_projectNav.to(st_projectName.words, { y: -22 }, 0).to(
       st_projectNameItalic.words,
       {
         y: -22,
       },
       0
     )
+  }
+
+  function hlsPlayerInit() {
+    const video = document.querySelector('.project-video')
+
+    if (!video) return
+
+    const videoSrc = video.getAttribute('data-hls-src')
+
+    if (Hls.isSupported()) {
+      const hls = new Hls()
+      hls.loadSource(videoSrc)
+      hls.attachMedia(video)
+
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        console.log('Video is ready to play')
+      })
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = videoSrc
+    }
   }
 
   mm.add(
@@ -67,8 +89,24 @@ const pageProjectTemplate = () => {
       let { isDesktop, isTablet } = context.conditions
       console.log(isDesktop, isTablet) // REMOVE THIS
       gsap.set('.page-wrapper', { clearProps: 'all' })
+
+      // Split text ----- continue here....
+
+      const tl_intro = gsap.timeline()
+
+      tl_intro.from(
+        '.project-video',
+        {
+          duration: 1.2,
+          clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
+          ease: 'circ.inOut',
+        },
+        0.4
+      )
     }
   )
+
+  hlsPlayerInit()
 }
 
 export default pageProjectTemplate
